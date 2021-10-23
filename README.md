d55c8ed369b95e31435d2b3c17cada6ca29efcdf
Michael Li <thisislimichael@gmail.com>
2021-10-23 16:59:04 -0700

    added comments to explain code

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Coding_Quiz_Prep
+
+## Purpose
+The purpose of this quiz is to help prep for coding interviews. 
+
+## Built With 
+* Javascript
+* HTML
+* CSS
+
+## Website
+https://limichael97.github.io/Coding_Quiz_Prep/
+
+## Contribution
+Michael Li
+
+### ©Michael Li 👨‍💻
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// Declaring Variables
+
 var timerEl = document.getElementById('countdown');
 var click = document.getElementById('start');
 var askQuestions = document.getElementById('title')
@@ -10,13 +12,18 @@ var correctAnswers = 0;
 var targetMain = document.getElementById('main');
 var form = document.getElementById('form');
 var highScores= [];
+
+/* 
+When page is opened, looks if there are any high scores in local storage and store it in a vairable,
+if not then 'noStorage' will just appear in console, and code will proceed.
+*/
 if (JSON.parse(localStorage.getItem("highscores")) == null) {
     console.log("noStorage");
 } else {
     highScores = JSON.parse(localStorage.getItem("highscores"))
 }
 
-
+// Variable to store array of questions, answers, and if true/false
 var questions = [
     {
      question:"Commonly used data types DO NOT include:",
@@ -125,6 +132,7 @@ var questions = [
     }
 ];
 
+// Dynamically making text for when quiz is over. Will tell user what their score is
 function finalText() {
     var done = document.getElementById("allDone");
     var doneAll = document.createElement("h1");
@@ -135,6 +143,10 @@ function finalText() {
     allDone.appendChild(finalScore);
 }
 
+/* 
+Starting timer. after each question, there is a counter (questionIndex) that increases by 1.
+When that counter equals the # of questions then quiz is over. Unless time reaches 0 or less first.
+*/
 function timerStart() {
     var countdown = setInterval(function() {
         if (timeLeft >= 0){
@@ -142,7 +154,7 @@ function timerStart() {
             timeLeft--
         } 
 
-        if ((questionIndex == questions.length) || (timeLeft===0)) {
+        if ((questionIndex == questions.length) || (timeLeft===0) || (timeLeft < 0)) {
             timerEl.textContent=""
             alert("End of Quiz");
             targetMain.classList.add("hidden")
@@ -153,12 +165,15 @@ function timerStart() {
     }, 1000);
 };
 
+
+// This function clears out the beginning screen when the button "Start Quiz" is clicked
 function clearScreen() {
     askQuestions.innerHTML = "";
     options.innerHTML="";
     btnGone.remove();
 };
 
+// The first question and options appears dynamically.
 function questionStart() {
     askQuestions.innerHTML = questions[questionIndex].question;
     for (i =0; i < 4; i++) {
@@ -171,12 +186,11 @@ function questionStart() {
     }
 };
 
-function checkAnswers() {
-    var varifyAnswer= document.createElement("p");
-    varifyAnswer.innerHTML = "Correct!";
-    options.appendChild(varifyAnswer);
-
-}
+/* 
+After the first question is answered, the rest of questions and options 
+are dynamically made here. The if/else if conditions will tell user if 
+their answer is wrong or correct by changing the text accordingly
+*/
 
 function nextQuestion(event) {
     questionIndex++
@@ -209,13 +223,8 @@ function nextQuestion(event) {
     }
 }
 
-
+// Save data to a variable, and stored in local.storage as a string
 function saveScore() {
-    // capture Results 
-    // capture user input
-    // create object using the users input and score
-    // stringify
-    // save to local storage
     var data = {
         initials: document.getElementById("initials").value,
         score: correctAnswers 
@@ -225,12 +234,18 @@ function saveScore() {
     localStorage.setItem("highscores", JSON.stringify(highScores))
 }
 
+/* 
+At the beginning, when the 'Start Quiz' button is pressed. The timer will start,
+screen will clear, and first question/options will show up
+
+*/
 function buttonStart() {
     timerStart()
     clearScreen()
     questionStart()
 };
 
+// When clicked the function will occur
 click.addEventListener("click", buttonStart)
 document.getElementById("save").addEventListener("click", saveScore)
 
